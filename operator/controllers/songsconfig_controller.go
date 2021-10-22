7f195a3e3b25361a6c5423832899decfb508c563
Paul <paulfbutler2016@gmail.com>
2021-10-22 09:16:10 -0700

    small update

--- operator/controllers/songsconfig_controller.go
+++ operator/controllers/songsconfig_controller.go
@@ -47,11 +47,11 @@ type SongsConfigReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *SongsConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// your logic here
+	log.Info("Hello World")
 
-	return ctrl.Result{}, nil
+	return ctrl.Result{Requeue: true}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
